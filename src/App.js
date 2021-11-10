7cf324e5ca93a40c1f4d0c46c376d95e6e87c17b
louisjmorgan <louis.morgan@hotmail.com>
2021-11-10 00:56:47 +0000

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
+  NavLink,
 } from 'react-router-dom';
+import Home from './Home';
+import Reels from './Reels';
+import MoreContent from './MoreContent';
+import Contact from './Contact';
 import MikeLoganAudio from './assets/mikeloganaudio.jpg';
+import MikeLoganAudio2 from './assets/mikeloganaudio2.jpg';
 import Soundcloud from './assets/soundcloud.png';
 import Instagram from './assets/instagram.png';
 import Twitter from './assets/twitter.png';
-import HeroImage from './assets/synth.gif';
+
 import './styles.css';
+import ScrollToTop from './ScrollToTop';
 
 const App = ({ title }) => {
-  const [isActive, setActive] = useState(false);
+  const heroRef = useRef();
+  const [isMobile, setMobile] = useState(false);
 
   const hamburger = document.querySelector('.hamburger');
   const navbar = document.querySelector('.navbar');
 
   function mobileMenu() {
-    setActive((prevState) => !prevState);
+    setMobile((prevState) => !prevState);
     console.log('toggled active');
   }
 
@@ -31,110 +38,140 @@ const App = ({ title }) => {
   function closeMenu() {
     hamburger.classList.remove('active');
     navbar.classList.remove('active');
+    setMobile(false);
   }
 
   navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
+  function handleDownBtn() {
+    heroRef.current.scrollIntoView({ behavior: 'smooth' });
+    console.log('shag birds');
+  }
+
   return (
-    <Router>
-      <nav>
-        <div className={`navbar ${isActive ? 'active' : ''}`}>
-          <ul className="navlinks">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/Reels">Reels</Link>
-            </li>
-            <li>
-              <Link to="/More-Content">More Content</Link>
-            </li>
-            <li>
-              <Link to="Contact">Contact</Link>
-            </li>
-          </ul>
-          <div className="logo">
-            <img src={MikeLoganAudio} alt="Mike Logan Audio Logo" />
+    <Router onUpdate={() => window.scrollTo(0, 0)}>
+      <div>
+        <nav name="nav">
+          <div className={`navbar ${isMobile ? 'active' : ''}`}>
+            <ul className="navlinks">
+              <li className="navlink">
+                <NavLink exact to="/" activeClassName="selected">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/reels" activeClassName="selected">
+                  Reels
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/more-content"
+                  activeClassName="selected"
+                >
+                  More Content
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" activeClassName="selected">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+            <div className="logo">
+              <img src={MikeLoganAudio} alt="Mike Logan Audio Logo" />
+            </div>
+            <ul className="social-buttons">
+              <a
+                href="https://soundcloud.com/mikeloganaudio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Soundcloud} alt="soundcloud" />
+              </a>
+              <a
+                href="https://www.instagram.com/mikeloganaudio/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Instagram} alt="instagram" />
+              </a>
+              <a
+                href="https://www.twitter.com/mikeloganaudio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Twitter} alt="twitter" />
+              </a>
+            </ul>
           </div>
-          <ul className="social-buttons">
-            <a href="https://soundcloud.com/mikeloganaudio">
-              <img src={Soundcloud} alt="soundcloud" />
-            </a>
-            <a href="https://www.instagram.com/mikeloganaudio/">
-              <img src={Instagram} alt="instagram" />
-            </a>
-            <a href="https://www.twitter.com/mikeloganaudio">
-              <img src={Twitter} alt="twitter" />
-            </a>
-          </ul>
-        </div>
-        <div className="mobile-nav">
-          <button
-            type="button"
-            className={`hamburger ${isActive ? 'active' : ''}`}
-            onClick={mobileMenu}
+          <div className="mobile-nav">
+            <button
+              type="button"
+              className={`hamburger ${isMobile ? 'active' : ''}`}
+              onClick={mobileMenu}
+            >
+              <span className="bar" />
+              <span className="bar" />
+              <span className="bar" />
+            </button>
+            <div className="mobile-logo">
+              <img
+                src={MikeLoganAudio2}
+                alt="Mike Logan Audio Logo"
+              />
+            </div>
+          </div>
+        </nav>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/reels">
+            <Reels />
+          </Route>
+          <Route path="/more-content">
+            <MoreContent />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/">
+            <Home handleDownBtn={handleDownBtn} ref={heroRef} />
+          </Route>
+        </Switch>
+      </div>
+
+      <footer className="footer">
+        <ul className="social-footer">
+          <a
+            href="https://soundcloud.com/mikeloganaudio"
+            target="_blank"
+            rel="noreferrer"
           >
-            <span className="bar" />
-            <span className="bar" />
-            <span className="bar" />
-          </button>
-          <div className="mobile-logo">
-            <img src={MikeLoganAudio} alt="Mike Logan Audio Logo" />
-          </div>
-        </div>
-      </nav>
-      <main>
-        <section className="hero">
-          <img src={HeroImage} alt="hero banner" />
-          <div className="hero-text">
-            <span className="introduction">
-              My name’s Mike, I’m a sound designer and composer for
-              video games.
-            </span>
-            <span className="introduction">
-              I recently graduated in Electronic Music Production from
-              University of West London.
-            </span>
-            <span className="introduction">
-              Feel free to check out my reels and other content, and
-              don’t hesitate to get in touch!
-            </span>
-            <span className="recommendation">
-              <p>
-                “It&apos;s been an absolute dream working with Mike.
-                His ability to grasp projects quickly coupled with his
-                flexibility and creative vision make him an invaluable
-                asset to the team. Mike has a rare, genuine passion
-                for his craft, with a tenacious and can-do approach
-                that he brings to the team. I would not hesitate to
-                work with Mike and can&apos;t recommend him enough for
-                anyone looking for professional and inspiring output.”
-              </p>
-              <p>- WWYD Films</p>
-            </span>
-          </div>
-        </section>
-        <section className="videos">
-          <span>Game Audio Sound Design Reel</span>
-          <iframe
-            title="vimeo-player"
-            src="https://player.vimeo.com/video/563007174?h=5ac6615590"
-            width="640"
-            height="360"
-            frameBorder="0"
-            allowFullScreen
-          />
-          <span>Game Audio Composition Reel 2021</span>
-          <iframe
-            title="vimeo-player"
-            src="https://player.vimeo.com/video/564597865?h=a24f152033"
-            width="640"
-            height="360"
-            frameBorder="0"
-            allowFullScreen
-          />
-        </section>
-      </main>
+            <img src={Soundcloud} alt="soundcloud" />
+          </a>
+          <a
+            href="https://www.instagram.com/mikeloganaudio/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Instagram} alt="instagram" />
+          </a>
+          <a
+            href="https://www.twitter.com/mikeloganaudio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Twitter} alt="twitter" />
+          </a>
+        </ul>
+      </footer>
+      <NavLink
+        to="/contact#nav"
+        className="footer-cta"
+        activeClassName="selected"
+      >
+        Get in touch!
+      </NavLink>
     </Router>
   );
 };
---
+++ src/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './styles.css';
+
+const Contact = () => {
+  return (
+    <main>
+      <span className="direct-contact">
+        Get in touch directly at&nbsp;
+        <a href="mailto:mikeloganaudio@gmail.com">
+          mikeloganaudio@gmail.com
+        </a>
+      </span>
+      <div className="form-container">
+        <form
+          action="https://formspree.io/f/mvodjjev"
+          method="POST"
+          className="contact-form"
+        >
+          <label htmlFor="_replyto">
+            Your email:
+            <input type="email" name="_replyto" />
+          </label>
+          <label htmlFor="message">
+            Your message:
+            <textarea name="message" />
+          </label>
+
+          <button type="submit" target="_blank" className="form-btn">
+            Send
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Contact;
