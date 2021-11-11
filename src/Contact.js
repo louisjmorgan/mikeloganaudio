4bfd4c43505b38f5b6ab9e825706f17ba226d82e
louisjmorgan <louis.morgan@hotmail.com>
2021-11-11 17:24:23 +0000

    post-launch tweaks

--- src/Contact.js
+++ src/Contact.js
@@ -4,31 +4,37 @@ import './styles.css';
 const Contact = () => {
   return (
     <main>
-      <span className="direct-contact">
-        Get in touch directly at&nbsp;
-        <a href="mailto:mikeloganaudio@gmail.com">
-          mikeloganaudio@gmail.com
-        </a>
-      </span>
-      <div className="form-container">
-        <form
-          action="https://formspree.io/f/mvodjjev"
-          method="POST"
-          className="contact-form"
-        >
-          <label htmlFor="_replyto">
-            Your email:
-            <input type="email" name="_replyto" />
-          </label>
-          <label htmlFor="message">
-            Your message:
-            <textarea name="message" />
-          </label>
+      <div className="contact-page">
+        <span className="direct-contact">
+          Get in touch directly:&nbsp;
+          <a href="mailto:mikeloganaudio@gmail.com">
+            mikeloganaudio@gmail.com
+          </a>
+        </span>
+        <div className="form-container">
+          <form
+            action="https://formspree.io/f/mvodjjev"
+            method="POST"
+            className="contact-form"
+          >
+            <label htmlFor="_replyto">
+              Your email:
+              <input type="email" name="_replyto" />
+            </label>
+            <label htmlFor="message">
+              Your message:
+              <textarea name="message" />
+            </label>
 
-          <button type="submit" target="_blank" className="form-btn">
-            Send
-          </button>
-        </form>
+            <button
+              type="submit"
+              target="_blank"
+              className="form-btn"
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
